988423436ba4387a1cd311fec020503e689218bc
Pavan Raj K G <nesarastudio0492@gmail.com>
2024-11-30 11:46:48 +0530

    v

--- Notebook/fetch_periodically.py
+++ Notebook/fetch_periodically.py
@@ -1,12 +1,6 @@
 import papermill as pm
 from datetime import datetime
+import os 
 
-notebooks = ["Notebook\fetch_periodically.py", "Notebook\data-explorer.ipynb"]
+print(os.path())
 
-for notebook in notebooks:
-    try:
-        output = f"output_{notebook.split('.')[0]}_{datetime.now().strftime('%Y%m%d%H%M%S')}.ipynb"
-        pm.execute_notebook(notebook, output)
-        print(f"Executed {notebook}, saved as {output}")
-    except Exception as e:
-        print(f"Failed to execute {notebook}: {e}")

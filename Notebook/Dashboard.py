ed6adde10aa569c88f24d82c58ef349c1984fb27
Pavan Raj K G <nesarastudio0492@gmail.com>
2024-12-06 14:56:57 +0530

    d

--- Notebook/Dashboard.py
+++ Notebook/Dashboard.py
@@ -1,14 +1,24 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from datetime import datetime 
+from datetime import datetime
+import time
 
 def main():
     st.title("Bengaluru Weather Dashboard")
-    data = pd.read_excel('https://raw.githubusercontent.com/pavanrajkg04/Weather_Dashboard/main/Notebook/data_table.xlsx')
-    st.write(datetime.now())
-    st.write("Latitute : ",round(data['lat'].iloc[-1],2), 'Longitute : ',round(data['lon'].iloc[-1],2))
-    st.line_chart(data=data,x='localtime',y='temp_c',x_label='Date',y_label='Temp C',color='#ffaa00',width=300,height=500,use_container_width=True)
 
+    # Load the data
+    data_url = 'https://raw.githubusercontent.com/pavanrajkg04/Weather_Dashboard/main/Notebook/data_table.xlsx'
+    data = pd.read_excel(data_url)
 
-main()
+    # Show current time and weather information
+    st.write("Current Time: ", datetime.now())
+    st.write("Latitude: ", round(data['lat'].iloc[-1], 2), 'Longitude: ', round(data['lon'].iloc[-1], 2))
+
+    # Line chart of temperature over time
+    st.line_chart(data=data, x='localtime', y='temp_c', use_container_width=True)
+
+# Main Streamlit app execution
+if __name__ == '__main__':
+    main()
+

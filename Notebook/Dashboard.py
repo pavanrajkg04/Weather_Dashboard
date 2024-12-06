5f5622204b5a4ed07321c4919fb182213d086e2a
Pavan Raj K G <nesarastudio0492@gmail.com>
2024-12-06 16:00:12 +0530

    f

--- Notebook/Dashboard.py
+++ Notebook/Dashboard.py
@@ -4,12 +4,21 @@ import numpy as np
 from datetime import datetime
 import time
 
+@st.cache_data
+def load_data(file):
+    df = pd.read_csv(file)
+    return df
+
+
 def main():
     st.title("Bengaluru Weather Dashboard")
+    
+    file = "https://raw.githubusercontent.com/pavanrajkg04/Weather_Dashboard/main/Notebook/data_table.xlsx"
+    data = load_data(file)
 
     # Load the data
-    data_url = 'https://raw.githubusercontent.com/pavanrajkg04/Weather_Dashboard/main/Notebook/data_table.xlsx'
-    data = pd.read_excel(data_url)
+    #data_url = 'https://raw.githubusercontent.com/pavanrajkg04/Weather_Dashboard/main/Notebook/data_table.xlsx'
+    #data = pd.read_excel(data_url)
 
     # Show current time and weather information
     st.write("Current Time: ", datetime.now())
@@ -21,4 +30,3 @@ def main():
 # Main Streamlit app execution
 if __name__ == '__main__':
     main()
-
